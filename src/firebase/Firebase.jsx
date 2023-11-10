1491ce0448c9d9d93f342bc232cf30cd0329cfc5
Anil parajuli <anilparajuli580@gmail.com>
2023-11-09 23:52:19 -0800

    creating env variable

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -5,12 +5,12 @@ import { getFirestore, collection } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrsiXcLfbOo5Xk7mgQkH8SI99Z0UbHnDk",
-  authDomain: "fimy-verse.firebaseapp.com",
-  projectId: "fimy-verse",
-  storageBucket: "fimy-verse.appspot.com",
-  messagingSenderId: "314433482229",
-  appId: "1:314433482229:web:7c7a97e59f46929cf504bc",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
@@ -20,3 +20,10 @@ export const movieRef = collection(db, "movies");
 export const reviewsRef = collection(db, "reviews");
 
 export default app;
+
+// apiKey: "AIzaSyDrsiXcLfbOo5Xk7mgQkH8SI99Z0UbHnDk",
+// authDomain: "fimy-verse.firebaseapp.com",
+// projectId: "fimy-verse",
+// storageBucket: "fimy-verse.appspot.com",
+// messagingSenderId: "314433482229",
+// appId: "1:314433482229:web:7c7a97e59f46929cf504bc",
